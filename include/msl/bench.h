19b58d1cfb19752dc1939b7d54f4c5b4c621d8be
martysama0134 <martysama0134@gmail.com>
2018-08-01 23:15:59 +0200

    +bench with evaluate outside

--- include/msl/bench.h
+++ include/msl/bench.h
@@ -8,17 +8,17 @@ namespace msl
 class bench
 {
 public:
-	template <class F, class... Args> bench(F && func, Args &&... args)
+	template <class F, class... Args> bench(F && func, Args &&... args, size_t tries = 10000)
 	{
-		std::cout << "Elapsed time: " << run(func, args...).count() << "ms\n";
-	}
-
-	template <class F, class... Args> auto run(F && func, Args &&... args)
-	{
-		auto start = std::chrono::steady_clock::now();
-		for (auto i = 0; i < 10000; i++)
-			std::invoke(std::forward<decltype(func)>(func), std::forward<Args>(args)...);
-		return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start);
+		std::cout << "Elapsed time: " << evaluate(func, args..., tries).count() << "ms\n";
 	}
 };
+
+template <class F, class... Args> auto evaluate(F && func, Args &&... args, size_t tries = 1)
+{
+	auto start = std::chrono::steady_clock::now();
+	for (auto i = 0; i < tries; i++)
+		std::invoke(std::forward<decltype(func)>(func), std::forward<Args>(args)...);
+	return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start);
+}
 } // namespace msl
