d2f8c2a5256eb7ff6c717855587d0168239bc9ad
martysama0134 <martysama0134@gmail.com>
2018-08-01 23:57:25 +0200

    -bench functional

--- include/msl/bench.h
+++ include/msl/bench.h
@@ -1,25 +1,24 @@
 #pragma once
 
 #include <chrono>
-#include <functional>
 #include <iostream>
 namespace msl
 {
-template <class F, class... Args> auto evaluate(size_t tries, F && func, Args &&... args)
+template <class F, class... Args> auto evaluate(size_t tries, F && func)
 {
 	auto start = std::chrono::steady_clock::now();
 	for (auto i = 0; i < tries; i++)
-		std::invoke(std::forward<decltype(func)>(func), std::forward<Args>(args)...);
+		func();
 	return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start);
 }
 
 class bench
 {
 public:
-	template <class F, class... Args> bench(F && func, Args &&... args)
+	template <class F, class... Args> bench(F && func)
 	{
 		constexpr auto tries = 10000;
-		std::cout << "Elapsed time: " << evaluate(tries, func, args...).count() << "ms\n";
+		std::cout << "Elapsed time: " << evaluate(tries, func).count() << "ms\n";
 	}
 };
 
