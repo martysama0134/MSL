747434eac6e55e3694e66a80846a88e04addae3e
martysama0134 <martysama0134@gmail.com>
2018-08-01 16:33:33 +0200

    +file_ptr read string_read

--- include/msl/file_ptr.h
+++ include/msl/file_ptr.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstdio>
+#include <vector>
 
 namespace msl
 {
@@ -17,7 +18,16 @@ public:
 	file_ptr(std::FILE * ptr) { m_ptr = ptr; }
 	~file_ptr() { std::fclose(m_ptr); }
 	std::FILE * operator*() { return m_ptr; }
+
 	std::FILE * get() { return m_ptr; }
+	void reset()
+	{
+		if (m_ptr)
+		{
+			std::fclose(m_ptr);
+			m_ptr = nullptr;
+		}
+	}
 
 	bool is_open() const { return m_ptr; }
 
@@ -28,6 +38,22 @@ public:
 		std::size_t filesize = std::ftell(m_ptr); // get filesize
 		std::fseek(m_ptr, cur, SEEK_SET); // go to old pos
 		return filesize;
+	}
+
+	std::vector<char> read(std::size_t n = 0)
+	{
+		if (n == 0) // read whole remaining file
+			n = this->size();
+		std::vector<char> buf(n);
+		fread(buf.data(), 1, buf.size(), m_ptr);
+		return buf;
+	}
+
+	std::string string_read(std::size_t n = 0)
+	{
+		auto vec = this->read(n);
+		vec.emplace_back('\0');
+		return std::string(vec.begin(), vec.end());
 	}
 };
 } // namespace msl
--- msl/test.cpp
+++ msl/test.cpp
@@ -9,11 +9,7 @@ int main()
 	msl::file_ptr p("test.txt");
 	std::cout << ((p.is_open()) ? "OPEN"s : "CLOSE"s) << '\n';
 	std::cout << p.size() << '\n';
-	{
-		char buf[100]{};
-		fread(buf, 1, sizeof buf, *p);
-		puts(buf);
-	}
+	std::cout << p.string_read() << '\n';
 	getchar();
 	getchar();
 	return 0;
