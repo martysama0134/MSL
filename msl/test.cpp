54fbfcaaa4e0ab83c879fab50735ccef0e59f9ef
martysama0134 <martysama0134@gmail.com>
2018-08-01 17:25:01 +0200

    +file_ptr tests comments

--- msl/test.cpp
+++ msl/test.cpp
@@ -7,16 +7,20 @@ int main()
 {
 	// file_ptr tests
 	{
-		msl::file_ptr p("test.txt");
+		msl::file_ptr p("test.txt"); // open test.txt on read-mode
+
 		std::cout << ((p.is_open()) ? "OPEN"s : "CLOSE"s) << '\n';
-		msl::check_assert(p.is_open());
+		msl::check_assert(p.is_open()); // check if open
+
 		std::cout << p.size() << '\n';
-		msl::check_assert(p.size() == 3);
+		msl::check_assert(p.size() == 3); // check its size
+
 		std::cout << p.string_read() << '\n';
 		std::cout << p.size() << '\n';
-		msl::check_assert(p.size() == 3);
+		msl::check_assert(p.size() == 3); // check its size after read
+
 		std::cout << p.remain_size() << '\n';
-		msl::check_assert(p.remain_size() == 0);
+		msl::check_assert(p.remain_size() == 0); // check its remaining size after read
 	}
 	getchar();
 	getchar();
